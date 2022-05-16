0f0f3ebfd6e4cbbd182fe511582506fa27d90ed0
kenjifushida <kenjifushida@gmail.com>
2022-05-16 14:31:19 +0900

    added header

---
+++ src/components/combobox.js
@@ -0,0 +1,10 @@
+import * as React from "react"
+import * as styles from "../styles/combobox.module.scss"
+
+const Combobox = ({}) => (
+    <input type="text" maxlength="12">
+
+    </input>
+)
+
+export default Combobox
